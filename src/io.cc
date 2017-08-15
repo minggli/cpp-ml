d616ed8210e45bf70bb1f8c1e18f3e86185852a4
Ming Li <ming.george.li@gmail.com>
2017-08-16 00:29:48 +0100

    new file

--- src/io.cc
+++ src/io.cc
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int readNumber()
+{
+  int n;
+  std::cout << "Please enter a number: " << std::endl;
+  std::cin >> n;
+  return n;
+}
+
+void writeAnswer(int x)
+{
+  std::cout << "The answer is :" << x << std::endl;
+}
 
 int main() {
     std::cout << "Enter a number: "; // ask user for a number
