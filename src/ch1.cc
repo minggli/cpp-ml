f53cb756c90fd22cdc1334eef221b1d05189d565
Ming Li <ming.george.li@gmail.com>
2017-08-16 00:58:16 +0100

    header guards

--- src/ch1.cc
+++ src/ch1.cc
@@ -2,11 +2,13 @@
 Write a single-file program (named main.cpp) that reads two separate integers
 from the user, adds them together, and then outputs the answer.
 */
-int readNumber();
-void writeAnswer(int x);
+#include "io.h"
 
-int main()
-{
+// forward declaration of functions
+// int readNumber();
+// void writeAnswer(int x);
+
+int main() {
   writeAnswer(readNumber() + readNumber());
   return 0;
 }
--- src/io.cc
+++ src/io.cc
@@ -1,22 +1,7 @@
-#include <iostream>
+#ifndef IO_H
+#define IO_H
 
-int readNumber()
-{
-  int n;
-  std::cout << "Please enter a number: " << std::endl;
-  std::cin >> n;
-  return n;
-}
+int readNumber();
+void writeAnswer(int x);
 
-void writeAnswer(int x)
-{
-  std::cout << "The answer is :" << x << std::endl;
-}
-
-int main() {
-    std::cout << "Enter a number: "; // ask user for a number
-    int x; // no need to initialize x since we're going to overwrite that value on the very next line
-    std::cin >> x; // read number from console and store it in x
-    std::cout << "You entered " << x << std::endl;
-    return 0;
-}
+#endif
