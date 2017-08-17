9f7f7d697e412484bbd55721b24dc59c96c2a83b
Ming Li <ming.george.li@gmail.com>
2017-08-17 18:16:10 +0100

    freeball

---
+++ gravity/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+namespace myConstants {
+    const double G(9.8);
+}
+#endif
---
+++ gravity/freefall.cc
@@ -0,0 +1,37 @@
+// toy programme where ball drops
+#include <iostream>
+#include "constants.h"
+
+uint16_t heightTower() {
+    std::cout << "Enter the initial height of the tower in meters: \n";
+    uint16_t height;
+    // 0 - 65535 meters for a tower
+    std::cin >> height;
+    return height;
+}
+
+float fallenDistance(uint16_t sec) {
+    return myConstants::G * sec * sec / 2;
+}
+
+void heightPrint(uint16_t sec, float h) {
+    // std::cout << std::setprecision(1)
+    if (h > 0) {
+    std::cout << "At " << sec << " seconds, the ball is at height: " << h <<
+                 " meters\n";
+    } else {
+        std::cout << "At " << sec << " seconds, the ball is on the ground.\n";
+    }
+}
+
+int main() {
+    const uint16_t hTower(heightTower());
+    uint16_t s(0);
+    float h;
+    do {
+        h = hTower - fallenDistance(s);
+        heightPrint(s, h);
+        s += 1;
+    } while (h > 0);
+    return 0;
+}
